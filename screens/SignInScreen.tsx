d65206ca7b4ecad9925e998713e2b057cfbc8552
Brice WeKeys <formation@wekeys.fr>
2026-05-27 19:46:21 +0200

    Ajout .env

--- screens/SignInScreen.tsx
+++ screens/SignInScreen.tsx
@@ -1,4 +1,5 @@
 import React, { useState } from 'react';
+import {Buttons} from '../components/buttons';
 import {
   Image,
   ImageBackground,
@@ -12,23 +13,63 @@ import {
 } from 'react-native';
 import { NavigationProp, ParamListBase } from '@react-navigation/native';
 import { SafeAreaView } from 'react-native-safe-area-context';
-
+import BACKEND_IP from '@env';
+ 
 type SignInScreenProps = {
   navigation: NavigationProp<ParamListBase>;
 };
 
 export default function SignInScreen({ navigation }: SignInScreenProps) {
 
+  const [login, setLogin] = useState<string>('');
+  const [password, setPassword] = useState<string>('');
+  
   const handleSubmit = () => {
-    navigation.navigate('TabNavigator', { screen: 'MyCollection' });
+    
+    const BACKEND_IP = process.env.BACKEND_IP
+    console.log(BACKEND_IP);
+    /* navigation.navigate('TabNavigator', { screen: 'MyCollection' }); */
   };
   return (
-    <SafeAreaView style={styles.container}>
-      <Text>Bienvenue sur mon App</Text>
-      <TouchableOpacity onPress={() => handleSubmit()}>
-        <Text>SIGN IN</Text>
-      </TouchableOpacity>
-    </SafeAreaView>
+        <ImageBackground source={require('../assets/Partager.png')} style={styles.background}>
+          <KeyboardAvoidingView
+            behavior={Platform.OS === 'ios' ? 'padding' : 'height'}
+            style={styles.container}
+          ><SafeAreaView style={styles.container}>
+      <Text>Se connecter</Text>
+      <View style={styles.bigctn}>
+      <View style={styles.loginctn}>
+        <Text>Nom d'utilisateur :</Text>
+        <TextInput
+            placeholder="FandeMoviez17"
+            autoCapitalize="none" // https://reactnative.dev/docs/textinput#autocapitalize
+            keyboardType="email-address" // https://reactnative.dev/docs/textinput#keyboardtype
+            textContentType="none" // https://reactnative.dev/docs/textinput#textcontenttype-ios
+            autoComplete="username" // https://reactnative.dev/docs/textinput#autocomplete-android
+            onChangeText={(value) => setLogin(value)}
+            value={login}
+            style={styles.loginInput}
+          />
+      </View>
+      <View style={styles.passwordctn}>
+        <Text>Mot de Passe :</Text>
+          <TextInput
+              placeholder="MonMotDePasseSecret123"
+              autoCapitalize="none" // https://reactnative.dev/docs/textinput#autocapitalize
+              keyboardType="email-address" // https://reactnative.dev/docs/textinput#keyboardtype
+              textContentType="password" // https://reactnative.dev/docs/textinput#textcontenttype-ios
+              autoComplete="password" // https://reactnative.dev/docs/textinput#autocomplete-android
+              secureTextEntry={true}
+              onChangeText={(value) => setPassword(value)}
+              value={password}
+              style={styles.loginInput}
+            />
+      </View>
+      </View>
+      <Buttons title='Se Connecter' onPress={handleSubmit}/>
+    </SafeAreaView></KeyboardAvoidingView>
+              </ImageBackground>
+
     
   );
 }
@@ -38,6 +79,32 @@ const styles = StyleSheet.create({
     flex: 1,
     alignItems: 'center',
     justifyContent: 'center',
-    backgroundColor: 'rgb(56, 7, 38)'
   },
+  background: {
+    flex: 1,
+    resizeMode: 'cover',
+    justifyContent: 'center',
+  },
+  bigctn: {
+    flex: 1,
+    justifyContent: 'space-between',
+    
+  },
+  loginctn: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    flexDirection: 'row'
+  },
+  loginInput: {
+    fontSize: 15,
+    backgroundColor: '#fff',
+    padding: 5
+  },
+  passwordctn: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    flexDirection: 'row'
+  }
 });
