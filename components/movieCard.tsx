539ec0f38f0a1c3cf677b8c29f80d9263ea3ae19
Brice WeKeys <formation@wekeys.fr>
2026-05-30 11:27:03 +0200

    Ajout component movieCard

---
+++ components/movieCard.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+import { useSafeAreaInsets } from 'react-native-safe-area-context';
+
+export default function movieCard(props: {clickable: boolean}) {
+  
+    return (
+        <></>
+    );
+}
+
+const styles = StyleSheet.create({
+   
+});
